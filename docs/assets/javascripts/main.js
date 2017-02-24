33e73618fabd8381dbddf35a657685a20dca5ab7
Jonathan Garvin <jon@5valleys.com>
2017-02-23 22:13:50 -0700

    more fancy banner

--- docs/assets/javascripts/main.js
+++ docs/assets/javascripts/main.js
@@ -1,7 +1,1 @@
 $(document).foundation();
-
-$('.top-bar').on('sticky.zf.stuckto:top', function(){
-  $(this).addClass('shrink');
-}).on('sticky.zf.unstuckfrom:top', function(){
-  $(this).removeClass('shrink');
-})
