c3c289800376f010eca7eb426d71f1b7d9739331
Jonathan Garvin <jon@5valleys.com>
2017-03-26 11:53:48 -0600

    update authenticity page and tweek footer

--- docs/assets/javascripts/main.js
+++ docs/assets/javascripts/main.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+// Hack for sticky images inside of tab-content panels that are
+// initially hidden.
+$('#authenticity-tabs').on('change.zf.tabs', function() {
+  sticky_image = $("div[data-tabs-content='authenticity-tabs'] div.tabs-panel.is-active div.sticky")
+  sticky_image.foundation('_calc', true);
+  sticky_image.css('top', 0);
+});
